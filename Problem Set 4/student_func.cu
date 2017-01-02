a1554de0d771386497254288af618910b775d224
Thorsten Kober <thorsten.kober@googlemail.com>
2017-01-03 00:37:36 +0100

    Implemented step 1, creating a histogram for each digit.

--- Problem Set 4/student_func.cu
+++ Problem Set 4/student_func.cu
@@ -3,10 +3,11 @@
 
 #include "utils.h"
 #include <thrust/host_vector.h>
+#include <stdio.h>
 
 /* Red Eye Removal
    ===============
-   
+
    For this assignment we are implementing red eye removal.  This is
    accomplished by first creating a score for every pixel that tells us how
    likely it is to be a red eye pixel.  We have already done this for you - you
@@ -43,12 +44,136 @@
  */
 
 
+__global__ void binary_histogram(
+  unsigned int * const d_input,
+  int input_size,
+  int item_count,
+  int *d_histograms,
+  int bit
+) {
+  // Initialize lokal histogram
+  int bins[2];
+  bins[0] = 0;
+  bins[1] = 0;
+
+  // The first item this thread is responsible for
+  int start_index = threadIdx.x * item_count;
+  // Process all items, this thread is thread is responsible for
+  for (int index = start_index; index < start_index + item_count; index++) {
+    // If the index exceeds the number of items, stop
+    if (index >= input_size) {
+      break;
+    }
+
+    // Otherwise...
+    unsigned int mask = 1 << bit;
+    unsigned int item = d_input[index];
+    // ...check whether the requested bit is set...
+    int bin = (mask & item) > 0;
+    // ...and increment the appropriate bin
+    bins[bin] = bins[bin] + 1;
+  }
+
+  // Finally copy the local histogram to the global list
+  int histogram_index = threadIdx.x * 2;
+  d_histograms[histogram_index] = bins[0];
+  d_histograms[histogram_index+1] = bins[1];
+}
+
+
+__global__ void simple_reduce_historgrams(
+  int *d_histograms,
+  int *d_global_histogram,
+  int n,
+  int bins
+) {
+  extern __shared__ int sdata[];
+  int thread = threadIdx.x;
+
+  // Copy all histograms to shared memory
+  for (int bin = 0; bin < bins; bin++) {
+    sdata[thread * bins + bin] = d_histograms[thread * bins + bin];
+  }
+  __syncthreads();
+
+  // Process list as a binary tree, bottom up
+  for (int k = 2; k <= n; k <<= 1) {
+    // If the current thread is part of the current level
+    // update the value by adding the value of the
+    // left child
+    if ((thread+1) % k == 0) {
+      // The distance to left child is the node distance on the current level
+      // devided by two
+      int offset = k>>1;
+      // Add all bins of the left child tot the corresponding bins of the thread
+      for (int bin = 0; bin < bins; bin++) {
+        sdata[thread * bins + bin] = sdata[thread * bins + bin] + sdata[(thread - offset) * bins + bin];
+      }
+    }
+    __syncthreads();
+  }
+
+  // The result in the root is at the last position of the list
+  if (thread == (n-1)) {
+    // Copy all bins
+    for (int bin = 0; bin < bins; bin++) {
+      d_global_histogram[bin] = sdata[thread * bins + bin];
+    }
+  }
+}
+
+
+void historgram_for_bit(
+  unsigned int *d_values,
+  int *d_result,
+  int value_count,
+  int bit
+) {
+  // Due to primitive global histogram implementation,
+  // only 1 block with 1024 threads
+  const int MAX_THREADS = 1024;
+  const int HISTOGRAM_COUNT = MAX_THREADS;
+  // The number of items each thread needs to process
+  const int ITEMS_PER_HISTOGRAM = (value_count / HISTOGRAM_COUNT) + 1;
+  // Number of bins, for binary digits --> 2
+  const int BIN_COUNT = 2;
+
+  int *d_histograms;
+  // Calculate HISTOGRAM_COUNT histograms, each containing
+  // the bins for ITEMS_PER_HISTOGRAM items
+  cudaMalloc((void **) &d_histograms, sizeof(int) * HISTOGRAM_COUNT * BIN_COUNT);
+  binary_histogram<<<1, HISTOGRAM_COUNT>>>(d_values, value_count, ITEMS_PER_HISTOGRAM, d_histograms, bit);
+
+  // Reduce the local histograms to one global
+  int shared_size = sizeof(int) * HISTOGRAM_COUNT * BIN_COUNT;
+  simple_reduce_historgrams<<<1, HISTOGRAM_COUNT, shared_size>>>(d_histograms, d_result, HISTOGRAM_COUNT, BIN_COUNT);
+
+  cudaFree(d_histograms);
+}
+
+
 void your_sort(unsigned int* const d_inputVals,
                unsigned int* const d_inputPos,
                unsigned int* const d_outputVals,
                unsigned int* const d_outputPos,
                const size_t numElems)
-{ 
-  //TODO
-  //PUT YOUR SORT HERE
+{
+  // The number if steps in the Radix sort,
+  // corresponding to the number of bits/digits
+  const int LENGHT = 8 * sizeof(unsigned int);
+
+  // Allocate device memory for a binary histogram
+  int *d_histogram;
+  cudaMalloc((void **) &d_histogram, sizeof(int) * 2);
+
+  // Process one radix step per bit/digit
+  for (int i = 0; i < LENGHT; i++) {
+
+    // Create a histogram for the occurrences of 0 and 1
+    // for the current bit/digit
+    historgram_for_bit(d_inputVals, d_histogram, numElems, i);
+
+  }
+
+  cudaFree(d_histogram);
 }
